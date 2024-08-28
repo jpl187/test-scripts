5f03c695291159f518e8024e731f41e514549959
dr.jpl <jonas.jpl@protonmail.com>
2024-08-28 17:44:08 +0200

    fixed VA files

--- VA-test-1.sh
+++ VA-test-1.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# Pfad zu den Postfix-Dateien
-POSTFIX_PATH="/pfad/zu/postfix/dateien"
-
-# Datei, die das letzte Überwachungsdatum speichert
-LAST_RUN_FILE="/var/tmp/last_clamav_scan"
-
-# Falls die Datei nicht existiert, erstelle sie und setze das aktuelle Datum
+POSTFIX_DIR_INC="/var/spool/postfix/incoming"
+CACHE="/var/log/Email-scan.log"
 if [ ! -f "$LAST_RUN_FILE" ]; then
-    date '+%Y-%m-%d %H:%M:%S' > "$LAST_RUN_FILE"
+    date '+%Y-%m-%d %H:%M:%S' > "$CACHE"
 fi
-
-# Finde alle Dateien, die nach dem letzten Lauf erstellt wurden
-NEW_FILES=$(find "$POSTFIX_PATH" -type f -newermt "$(cat $LAST_RUN_FILE)")
-
-# Aktualisiere das Datum des letzten Laufs
-date '+%Y-%m-%d %H:%M:%S' > "$LAST_RUN_FILE"
-
-# Scanne die neuen Dateien mit ClamAV
+NEW_FILES=$(find "$POSTFIX_DIR_INC" -type f -newermt "$(cat $CACHE)")
+date '+%Y-%m-%d %H:%M:%S' > "$CACHE"
 for FILE in $NEW_FILES; do
     clamscan "$FILE"
 done
