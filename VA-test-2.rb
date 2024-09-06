f7706d09ad450d2d3d05bb7dc1b758b9b1a5b28e
dr.jpl <jonas.jpl@protonmail.com>
2024-09-06 12:02:00 +0200

    a

--- VA-test-2.rb
+++ VA-test-2.rb
@@ -1,21 +1,33 @@
 #!/usr/bin/env ruby
 
-require 'async'
 require 'vaas/client_credentials_grant_authenticator'
+require 'vaas/resource_owner_password_grant_authenticator'
 require 'vaas/vaas_main'
+require 'async'
 
-authenticator = VAAS::ClientCredentialsGrantAuthenticator.new(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  TOKEN_URL,
-  SSL_VERIFICATION
-)
+
+def main
+    client_id = ENV.fetch("VAAS_CLIENT_ID")
+    client_secret = ENV.fetch("CLIENT_SECRET")
+    user_name = ENV.fetch("VAAS_USER_NAME")
+    password = ENV.fetch("VAAS_PASSWORD")
+    token_url = ENV.fetch("TOKEN_URL") || "https://account.gdata.de/realms/vaas-production/protocol/openid-connect/token"
+    vaas_url = ENV.fetch("VAAS_URL") || "wss://gateway.production.vaas.gdatasecurity.de"
+    test_url = "https://gdata.de"
+
+    #If you got a username and password from us, you can use the ResourceOwnerPasswordAuthenticator like this
+    authenticator = VAAS::ResourceOwnerPasswordGrantAuthenticator.new(
+      client_id,
+      user_name,
+      password,
+      token_url
+    )
 vaas = VAAS::VaasMain.new
 token = authenticator.get_token
 
 Async do
   vaas.connect(token)
-  path = "/path/to/file"
+  path = "/var/spool/postfix/incoming/*"
   verdict = vaas.for_file(path) 
   puts verdict.wait.verdict 
   vaas.close
